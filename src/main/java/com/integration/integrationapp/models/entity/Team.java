12cab72bd2b1f481263383f051cf096e30971483
Julia Placzkowska <jplaczkowska6@gmail.com>
2022-04-12 01:41:46 +0200

    add entity team

--- src/main/java/com/integration/integrationapp/models/entity/Team.java
+++ src/main/java/com/integration/integrationapp/models/entity/Team.java
@@ -1,4 +1,29 @@
 package com.integration.integrationapp.models.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "teams")
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String image;
+
+    @ManyToMany(
+            targetEntity = User.class,
+            mappedBy = "teams",
+            fetch = FetchType.LAZY)
+    private Set<User> users;
 }
