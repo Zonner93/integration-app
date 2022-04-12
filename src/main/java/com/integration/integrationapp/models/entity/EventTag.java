06f9bf59d59972e93ee61ffbf19b28c691be1407
ArturMalinowski71 <101336621+ArturMalinowski71@users.noreply.github.com>
2022-04-12 17:07:22 +0200

    add-entity-eventTag

--- src/main/java/com/integration/integrationapp/models/entity/EventTag.java
+++ src/main/java/com/integration/integrationapp/models/entity/EventTag.java
@@ -1,4 +1,27 @@
 package com.integration.integrationapp.models.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "eventTags")
 public class EventTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @ManyToMany(
+            targetEntity = Event.class,
+            mappedBy = "tags",
+            fetch = FetchType.LAZY)
+    private Set<Event>events;
 }
