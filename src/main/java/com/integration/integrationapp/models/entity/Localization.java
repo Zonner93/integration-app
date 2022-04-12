4d0f37c635d8c654ff1c5e7db9ee437bf9b49cd6
ArturMalinowski71 <101336621+ArturMalinowski71@users.noreply.github.com>
2022-04-12 16:27:06 +0200

    add entity Localization

--- src/main/java/com/integration/integrationapp/models/entity/Localization.java
+++ src/main/java/com/integration/integrationapp/models/entity/Localization.java
@@ -1,4 +1,23 @@
 package com.integration.integrationapp.models.entity;
 
+import lombok.*;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "loclailzations")
 public class Localization {
+
+    private String name;
+    private String street;
+    private String city;
+    private String postalCode;
 }
