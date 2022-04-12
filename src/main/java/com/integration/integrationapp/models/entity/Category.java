7c950344a44994bf1199e93e7ddd7330aaee4c68
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-04-12 17:02:53 +0200

    add id attribute

--- src/main/java/com/integration/integrationapp/models/entity/Category.java
+++ src/main/java/com/integration/integrationapp/models/entity/Category.java
@@ -13,6 +13,11 @@ import java.util.Set;
 @Entity
 @Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String imageURL;
 
