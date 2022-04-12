e34d69dd302a247a71ff05064b8e571960d665e5
Julia Placzkowska <jplaczkowska6@gmail.com>
2022-04-12 12:43:40 +0200

    fix

--- src/main/java/com/integration/integrationapp/models/mapper/EventMapper.java
+++ src/main/java/com/integration/integrationapp/models/mapper/EventMapper.java
@@ -4,12 +4,11 @@ import com.integration.integrationapp.models.dto.BadgeDto;
 import com.integration.integrationapp.models.dto.CategoryDto;
 import com.integration.integrationapp.models.dto.EventDto;
 import com.integration.integrationapp.models.dto.UserDto;
-import com.integration.integrationapp.models.entity.Badge;
-import com.integration.integrationapp.models.entity.Event;
-import com.integration.integrationapp.models.entity.User;
+import com.integration.integrationapp.models.entity.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -18,12 +17,47 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class EventMapper {
 
+    private final CategoryMapper categoryMapper;
+    private final LocationMapper locationMapper;
+    private final UserMapper userMapper;
+    private final BadgeMapper badgeMapper;
 
     private List<CategoryDto> getListOfCategoriesDto(Event event) {
         return event.getCategories().stream()
-                .map(badge -> badgeMapper.entityToDto(badge))
+                .map(categoryMapper::entityToDto)
                 .collect(Collectors.toList());
     }
+
+    private List<Category> getListOfCategories(EventDto event) {
+        return event.getCategoriesDto().stream()
+                .map(categoryMapper::dtoToEntity)
+                .collect(Collectors.toList());
+    }
+
+    private List<UserDto> getListOfUsersDto(Event event) {
+        return event.getParticipants().stream()
+                .map(userMapper::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+    private List<User> getListOfUsers(EventDto event) {
+        return event.getUsers().stream()
+                .map(userMapper::dtoToEntity)
+                .collect(Collectors.toList());
+    }
+
+    private List<BadgeDto> getListOfBadgesDto(Event event) {
+        return event.getRewards().stream()
+                .map(badgeMapper::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+    private List<Badge> getListOfBadges(EventDto event) {
+        return event.getBadges().stream()
+                .map(badgeMapper::dtoToEntity)
+                .collect(Collectors.toList());
+    }
+
 
     public Event dtoToEntity(EventDto eventDto) {
         if (eventDto == null) {
@@ -34,14 +68,14 @@ public class EventMapper {
         if (Objects.nonNull(eventDto.getId())) {
             builder.id(eventDto.getId());
         }
-        if (Objects.nonNull(eventDto.getCategory())) {
-            builder.categories(eventDto.getCategory());
+        if (Objects.nonNull(eventDto.getCategoriesDto())) {
+            builder.categories(new HashSet<>(getListOfCategories(eventDto)));
         }
         if (Objects.nonNull(eventDto.getName())) {
             builder.eventName(eventDto.getName());
         }
         if (Objects.nonNull(eventDto.getPlace())) {
-            builder.location(eventDto.getPlace());
+            builder.location(locationMapper.dtoToEntity(eventDto.getPlace()));
         }
         if (Objects.nonNull(eventDto.getDescription())) {
             builder.description(eventDto.getDescription());
@@ -53,20 +87,18 @@ public class EventMapper {
             builder.dateEnd(eventDto.getDateEnd());
         }
         if (Objects.nonNull(eventDto.getUsers())) {
-            builder.participants(eventDto.getUsers());
+            builder.participants(new HashSet<>(getListOfUsers(eventDto)));
         }
         if (Objects.nonNull(eventDto.getBadges())) {
-            builder.rewards(eventDto.getBadges());
+            builder.rewards(new HashSet<>(getListOfBadges(eventDto)));
         }
         if (Objects.nonNull(eventDto.getStatus())) {
             builder.status(eventDto.getStatus());
         }
         if (Objects.nonNull(eventDto.getHost())) {
-            builder.host(eventDto.getHost());
+            builder.host(userMapper.dtoToEntity(eventDto.getHost()));
         }
-        if (Objects.nonNull(eventDto.getTags())) {
-            builder.tags(eventDto.getTags());
-        }
+
         return builder.build();
 
     }
@@ -81,13 +113,15 @@ public class EventMapper {
             builderDto.id(event.getId());
         }
         if (Objects.nonNull(event.getCategories())) {
-            builderDto.category(event.getCategories());
+            builderDto.categoriesDto(
+                    new HashSet<>(getListOfCategoriesDto(event))
+            );
         }
         if (Objects.nonNull(event.getEventName())) {
             builderDto.name(event.getEventName());
         }
         if (Objects.nonNull(event.getLocation())) {
-            builderDto.place(event.getLocation());
+            builderDto.place(locationMapper.entityToDto(event.getLocation()));
         }
         if (Objects.nonNull(event.getDescription())) {
             builderDto.description(event.getDescription());
@@ -99,19 +133,24 @@ public class EventMapper {
             builderDto.dateEnd(event.getDateEnd());
         }
         if (Objects.nonNull(event.getParticipants())) {
-            builderDto.users(event.getParticipants());
+            builderDto.users(getListOfUsersDto(event));
         }
         if (Objects.nonNull(event.getRewards())) {
-            builderDto.badges(event.getRewards());
+            builderDto.badges(getListOfBadgesDto(event));
         }
         if (Objects.nonNull(event.getStatus())) {
             builderDto.status(event.getStatus());
         }
         if (Objects.nonNull(event.getHost())) {
-            builderDto.host(event.getHost());
+            builderDto.host(userMapper.entityToDto(event.getHost()));
         }
         if (Objects.nonNull(event.getTags())) {
-            builderDto.tags(event.getTags());
+            builderDto.tags(event
+                            .getTags()
+                            .stream()
+                            .map(EventTag::getName)
+                            .collect(Collectors.toList())
+            );
         }
         return builderDto.build();
 
