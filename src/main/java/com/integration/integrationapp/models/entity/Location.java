5f9706178431915d51269fc8923e1e7985b56861
Julia Placzkowska <jplaczkowska6@gmail.com>
2022-04-12 03:06:24 +0200

    final fix

--- src/main/java/com/integration/integrationapp/models/entity/Location.java
+++ src/main/java/com/integration/integrationapp/models/entity/Location.java
@@ -3,7 +3,6 @@ package com.integration.integrationapp.models.entity;
 import lombok.*;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Getter
@@ -13,9 +12,9 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @Builder
 @Table(name = "localizations")
-public class Localization {
+public class Location {
 
-    private String name;
+    private String locationName;
     private String street;
     private String city;
     private String postalCode;
