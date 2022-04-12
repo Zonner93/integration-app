222ee902faf732ca15e7070deb8b189a6a49f87f
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-04-12 16:49:13 +0200

    add category entity

--- src/main/java/com/integration/integrationapp/models/entity/Category.java
+++ src/main/java/com/integration/integrationapp/models/entity/Category.java
@@ -1,4 +1,25 @@
 package com.integration.integrationapp.models.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "categories")
 public class Category {
+    private String name;
+    private String imageURL;
+
+    @ManyToMany
+    @JoinTable(
+            name = "category_user",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<Event> events;
 }
